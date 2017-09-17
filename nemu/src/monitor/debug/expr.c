f86e51b452e5cf0c133a7a5f309d1dfe96a607a3
tracer-ics2017 <tracer@njuics.org>
2017-09-16 20:20:43 -0700

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:20:43 up 15 min,  1 user,  load average: 0.02, 0.19, 0.29 1acac4aad66518197e6771a867d393359d603022

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -28,7 +28,7 @@ static struct rule {
   {"\\)", TK_rpar},          // right parenthesis
   {"\\(", TK_lpar},          // left parenthesis
   {"/", TK_DIV},         // divide
-  {"*", TK_TIMES},         // times
+  {"\\*", TK_TIMES},         // times
   {"-", TK_MINUS},         // minus
   {" +", TK_NOTYPE},    // spaces
   {"\\+", TK_PLUS},         // plus
