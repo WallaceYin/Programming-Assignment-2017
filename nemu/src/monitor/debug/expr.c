8d12eec0c379354f41c1ac02e0afcd34a35ca6a1
tracer-ics2017 <tracer@njuics.org>
2017-09-19 19:34:32 -0700

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-33-generic #37~16.04.1-Ubuntu SMP Fri Aug 11 14:07:24 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:34:32 up  2:18,  1 user,  load average: 0.05, 0.07, 0.05 70dea40d4ee83b72704d44b1d63cb3d6fe436338

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -184,8 +184,6 @@ static bool make_token(char *e) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
         position += substr_len;
 	if (rules[i].token_type == TK_NOTYPE)
 		break;
