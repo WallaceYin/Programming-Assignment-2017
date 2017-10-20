f51e9c73d44680fa175d7dd91e11c97edf2a9328
tracer-ics2017 <tracer@njuics.org>
2017-10-20 00:44:38 -0700

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  00:44:38 up  2:45,  1 user,  load average: 0.53, 0.67, 0.65 aa8edbc8e6a1708b87433280705a79898f0e119

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -10,8 +10,6 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_S, CC_NS, CC_P,  CC_NP,
     CC_L, CC_NL, CC_LE, CC_NLE
   };
-  Log("subcode is 0x%x",subcode);
-  Log("CC_E is 0x%x",CC_E);
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
@@ -78,7 +76,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
-
+  Log("success");
   if (invert) {
     rtl_xori(dest, dest, 0x1);
   }
