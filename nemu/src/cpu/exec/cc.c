1708eb8fd4ec9e8b436b9a29c1588a951fe71a68
tracer-ics2017 <tracer@njuics.org>
2017-10-20 00:43:31 -0700

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  00:43:31 up  2:44,  1 user,  load average: 1.09, 0.77, 0.68 10d37731c967bcc987a67d3dd374e64c00f887d

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -11,6 +11,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_L, CC_NL, CC_LE, CC_NLE
   };
   Log("subcode is 0x%x",subcode);
+  Log("CC_E is 0x%x",CC_E);
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
