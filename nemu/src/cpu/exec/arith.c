dcf0e937b7de227e4f6516e91bbbb1fbab20bdb4
tracer-ics2017 <tracer@njuics.org>
2017-10-23 20:21:39 -0700

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  20:21:39 up  2:47,  1 user,  load average: 1.51, 0.87, 0.62 de030485b8c87b797675aab29a2f81d6d2875287

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -21,9 +21,7 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  rtl_sext(&t0, &id_src->val, id_src->width);
-  rtl_sext(&t1, &id_dest->val, id_dest->width);
-  rtl_sub(&t2, &t1, &t0);
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t2);
 
   rtl_update_ZFSF(&t2, id_dest->width);
@@ -42,9 +40,7 @@ make_EHelper(sub) {
 }
 
 make_EHelper(cmp) {
-  rtl_sext(&t0, &id_src->val, id_src->width);
-  rtl_sext(&t1, &id_dest->val, id_dest->width);
-  rtl_sub(&t2, &t1, &t0);
+  rtl_sub(&t2, &id_dest->val, &id_src->val);
   Log("in cmp id_dest = 0x%x and id_src->val = 0x%x", id_dest->val, id_src->val);
   rtl_update_ZFSF(&t2, id_dest->width);
 
@@ -58,9 +54,6 @@ make_EHelper(cmp) {
   rtl_xor(&t1, &id_dest->val, &t2);
   rtl_and(&t0, &t0, &t1);
   rtl_msb(&t0, &t0, id_dest->width);
-  /*rtl_xori(&t1, &id_src->val, 0x80000000);
-  rtl_eq0(&t1, &t1);
-  rtl_or(&t0, &t0, &t1);*/
   rtl_set_OF(&t0);
 
   Log("OF = 0x%x",t0);
