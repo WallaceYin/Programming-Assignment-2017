186c8587d3d91b3b83241bbc32a714573aca4002
tracer-ics2017 <tracer@njuics.org>
2017-10-25 17:49:00 -0700

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  17:49:00 up 21 min,  1 user,  load average: 0.33, 0.33, 0.34 52dc88fcc01a4563ba10cca5211b706a2f5f053

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -165,6 +165,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
   cpu.esp = cpu.esp - 4;
+  Log("src1=%08x\n",*src1);
   rtl_sm(&cpu.esp, 4, src1);
 }
 
