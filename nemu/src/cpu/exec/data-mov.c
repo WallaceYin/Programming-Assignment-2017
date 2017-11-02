68fa9ecd72eb768ce3304f3aaacf365d01e6c78e
tracer-ics2017 <tracer@njuics.org>
2017-11-01 18:10:49 -0700

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-37-generic #41~16.04.1-Ubuntu SMP Fri Oct 6 22:42:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  18:10:49 up  1:11,  1 user,  load average: 0.88, 0.78, 0.76 fbac6bdd9ac4e9c316e9ac183b3a3297b1bb3a3

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -149,7 +149,6 @@ make_EHelper(movsx) {
 
 make_EHelper(movzx) {
   id_dest->width = decoding.is_operand_size_16 ? 2 : 4;
-  Log("movzx id_src->val = 0x%x",id_dest->val);
   operand_write(id_dest, &id_src->val);
   print_asm_template2(movzx);
 }
