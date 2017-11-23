26e2c1afe1b4ae0734a65ca014901ea7f9efe220
tracer-ics2017 <tracer@njuics.org>
2017-11-23 03:50:49 -0800

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-38-generic #42~16.04.1-Ubuntu SMP Tue Oct 10 16:32:20 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  03:50:49 up  5:36,  1 user,  load average: 1.59, 0.93, 0.75 894ac5695f66c3071d5e2111c4309ef4bb109cb1

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -9,8 +9,8 @@ void extern raise_intr(uint8_t NO, vaddr_t ret_addr) {
    t0 = ret_addr;
    rtl_push(&t0);
    uint32_t offset_L, offset_H, offset;
-   offset_L = vaddr_read(cpu.IDTR.base + 8*(NO-1), 2);
-   offset_H = vaddr_read(cpu.IDTR.base + 8*(NO-1) + 6, 2);
+   offset_L = vaddr_read(cpu.IDTR.base + 8*(NO), 2);
+   offset_H = vaddr_read(cpu.IDTR.base + 8*(NO) + 6, 2);
    offset = (offset_H << 16) + offset_L;
    decoding.is_jmp = 1;
    decoding.jmp_eip = offset;
