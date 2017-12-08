5e5da2823e0bbd4104db40ed3550bb7744f75ff2
tracer-ics2017 <tracer@njuics.org>
2017-12-08 03:35:32 -0800

    >  compile 161180162 wallaceyin Linux ubuntu 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  03:35:32 up 48 min,  1 user,  load average: 0.18, 0.29, 0.32 61c127bc7830777361e483f62c746051d6774a3e

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -10,90 +10,38 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     CC_S, CC_NS, CC_P,  CC_NP,
     CC_L, CC_NL, CC_LE, CC_NLE
   };
+
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
-    {
-      rtl_get_OF(&t0);
-      if (t0)
-        rtl_li(dest, 1);
-      else
-        rtl_li(dest, 0);
-      break;
-    }
-
-    case CC_B:
-    {
-      rtl_get_CF(&t0);
-      if (t0)
-        rtl_li(dest, 1);
-      else
-        rtl_li(dest, 0);
-      break;
-    }
-
-    case CC_E:
-    {
-      rtl_get_ZF(&t0);
-      if (t0)
-        rtl_li(dest, 1);
-      else
-        rtl_li(dest, 0);
+        *dest=cpu.eflags.OF;
         break;
-    }
-
+    case CC_B:
+        *dest=cpu.eflags.CF;
+        break;
+    case CC_E:
+        *dest=cpu.eflags.ZF;
+        break;
     case CC_BE:
-    {
-      rtl_get_CF(&t0);
-      rtl_get_ZF(&t1);
-      rtl_or(&t2, &t0, &t1);
-      if (t2)
-        rtl_li(dest, 1);
-      else
-        rtl_li(dest, 0);
-      break;
-    }
-
+        *dest=(cpu.eflags.CF|cpu.eflags.ZF);
+        break;
     case CC_S:
-    {
-      rtl_get_SF(&t0);
-      if (t0)
-        rtl_li(dest, 1);
-      else
-        rtl_li(dest, 0);
-      break;
-    }
-
+        *dest=cpu.eflags.SF;
+        break;
     case CC_L:
-    {
-      rtl_get_SF(&t0);
-      rtl_get_OF(&t1);
-      rtl_xor(&t2, &t0, &t1);
-      if (t2)
-        rtl_li(dest, 1);
-      else
-        rtl_li(dest, 0);
-      break;
-    }
-
+        *dest=(cpu.eflags.SF!=cpu.eflags.OF);
+        break;
     case CC_LE:
-    {
-      rtl_get_SF(&t0);
-      rtl_get_OF(&t1);
-      rtl_xor(&t0, &t0, &t1);
-      rtl_get_ZF(&t1);
-      rtl_or(&t2, &t0, & t1);
-      if (t2)
-        rtl_li(dest, 1);
-      else
-        rtl_li(dest, 0);
-      break;
-    }
-
+        *dest=((cpu.eflags.SF!=cpu.eflags.OF)|cpu.eflags.ZF);
+        break;
+      //TODO();
     default: panic("should not reach here");
+        break;
     case CC_P: panic("n86 does not have PF");
+        break;
   }
+
   if (invert) {
     rtl_xori(dest, dest, 0x1);
   }
