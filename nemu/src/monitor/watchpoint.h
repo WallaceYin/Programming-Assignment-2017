25aba7decf13689617237b6ec634b28d74b59145
tracer-ics2017 <tracer@njuics.org>
2017-12-08 04:05:58 -0800

    >  compile 161180162 wallaceyin Linux ubuntu 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  04:05:58 up  1:19,  1 user,  load average: 0.23, 0.19, 0.29 37c502789df38de6237082308a31e034acf0cc47

---
+++ nemu/src/monitor/watchpoint.h
@@ -0,0 +1,24 @@
+#ifndef __WATCHPOINT_H__
+#define __WATCHPOINT_H__
+
+#include "common.h"
+
+typedef struct watchpoint {
+  int NO;
+  uint32_t Val;
+  char exp[32];
+  struct watchpoint *next;
+  /* TODO: Add more members if necessary */
+
+
+} WP;
+
+WP* new_up();
+
+void free_wp(WP *wp);
+
+WP* print_wp(WP *wp);
+
+bool check_wp();
+
+#endif
