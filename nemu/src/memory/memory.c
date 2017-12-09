a79a6158678367cf1ad4a3006881f4af70472f1a
tracer-ics2017 <tracer@njuics.org>
2017-12-08 19:35:40 -0800

    >  run 161220160 wallaceyin Linux ubuntu 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:35:40 up  4:04,  1 user,  load average: 0.23, 0.42, 0.52 c57ca2bb12b7ed376d13f74776aa83082d7f2e9c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,9 +1,10 @@
 #include "nemu.h"
 #include "device/mmio.h"
+
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
-    Warning(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
+    Assert(addr < PMEM_SIZE, "physical address(0x%08x) is out of bound", addr); \
     guest_to_host(addr); \
     })
 
@@ -12,19 +13,24 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  int ret=is_mmio(addr);
-  if(ret==-1)
-      return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
-  else
-      return mmio_read(addr, len, ret);
+  uint32_t data_read;
+  if (is_mmio(addr) == -1)
+    data_read = pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  else{
+    data_read = mmio_read(addr, len,is_mmio(addr));
+  }
+
+  return data_read;
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  int ret=is_mmio(addr);
-  if(ret==-1)
-      memcpy(guest_to_host(addr), &data, len);
+  if (is_mmio(addr) == -1)
+    memcpy(guest_to_host(addr), &data, len);
   else
-      mmio_write(addr, len, data, ret);
+  {
+    mmio_write(addr, len, data, is_mmio(addr));
+  }
+
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
