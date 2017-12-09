418d90244ff2266b1491832531941d467afc7f70
tracer-ics2017 <tracer@njuics.org>
2017-12-08 19:28:32 -0800

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  19:28:32 up  3:57,  1 user,  load average: 0.85, 0.76, 0.63 cd07679d5972c2338afae74b21680036d4de477a

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -84,7 +84,7 @@ static inline void restart() {
   /* Set the initial instruction pointer. */
   cpu.eip = ENTRY_START;
   cpu.eflags_init = EFLAGS_START;
-  cpu.cs = 8;
+  cpu.cs = 0x8;
 
 #ifdef DIFF_TEST
   init_qemu_reg();
