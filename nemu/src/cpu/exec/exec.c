6eba8903175ae8f4db7d4c8effe36f25f53cf894
tracer-ics2017 <tracer@njuics.org>
2017-12-24 22:23:46 -0800

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  22:23:46 up  1:09,  1 user,  load average: 0.18, 0.17, 0.25 ecced9e244f8d3b08f557a59397002b4969faf04

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -147,7 +147,7 @@ opcode_entry opcode_table [512] = {
   /* 0x14 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x18 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x1c */	EMPTY, EMPTY, EMPTY, EMPTY,
-  /* 0x20 */	IDEX(r, mov_cr2r), EMPTY, IDEX(r, mov_r2cr), EMPTY,
+  /* 0x20 */	IDEXW(G2E, mov_cr2r, 4), EMPTY, IDEXW(E2G, mov_r2cr, 4), EMPTY,
   /* 0x24 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x28 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x2c */	EMPTY, EMPTY, EMPTY, EMPTY,
