64dbf8745de14231520df9116bb3f34e44105c33
tracer-ics2017 <tracer@njuics.org>
2017-12-27 03:47:02 -0800

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  03:47:03 up  1:01,  1 user,  load average: 0.53, 0.33, 0.31 dd27993692102ebdb2269c5bdab0ccdec5bd817c

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -49,6 +49,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
     uint32_t paddr_2 = page_translate((uint32_t)addr + (0x1000 - (uint32_t)(addr & 0xfff)));
     uint32_t len_1 = 0x1000 - (uint32_t)(addr & 0xfff);
     uint32_t len_2 = len - len_1;
+    assert(0);
     return paddr_read(paddr_1, len_1) | paddr_read(paddr_2, len_2) << (8 * len_1);
   }
   else
