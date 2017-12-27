e40df8895efad5c21ed1a12d8b07c7977c815179
tracer-ics2017 <tracer@njuics.org>
2017-12-27 03:40:12 -0800

    >  compile 161220160 wallaceyin Linux ubuntu 4.10.0-42-generic #46~16.04.1-Ubuntu SMP Mon Dec 4 15:57:59 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux  03:40:12 up 55 min,  1 user,  load average: 0.37, 0.28, 0.30 f99198176cb6afdd9ee1b485f1a3f75668d42409

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -56,6 +56,7 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
     uint32_t paddr = page_translate(addr);
     return paddr_read(paddr, len);
   }
+  assert(0);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
@@ -72,6 +73,7 @@ void vaddr_write(vaddr_t addr, int len, uint32_t data) {
     uint32_t len_2 = len - len_1;
     paddr_write(paddr_1, len_1, (data << (32 - 8 * len_1)) >> (32 - 8 * len_1));
     paddr_write(paddr_2, len_2, (data >> (8 * len_1)));
+    assert(0);
   }
   else
   {
@@ -82,7 +84,7 @@ void vaddr_write(vaddr_t addr, int len, uint32_t data) {
 
 paddr_t page_translate(vaddr_t addr) {
   uint32_t Dir_entry;
-  Dir_entry = paddr_read(((PDX(addr) << 2) | (cpu.cr3.addr << 12)), 4);
+  Dir_entry = paddr_read(((PDX(addr) << 2) | ((cpu.cr3.addr & 0xfffff000) << 12)), 4);
   if ((Dir_entry & 0x001) == 0)
     Log("addr = %x",addr);
   assert((Dir_entry & 0x001) > 0);
